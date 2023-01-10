2d01245753fd9c87eb98c06b754f3f6020015aab
Joana Martínez <martinez.lopez.joana@alumnat.copernic.cat>
2023-01-10 22:45:50 +0100

    Second commit, side bar working

--- code/js/index.js
+++ code/js/index.js
@@ -1,11 +1,28 @@
 /* var contents = $('#contents')[0]; //returns a HTML DOM Object */
 
 function openNav() {
-    $(".side-nav-bar").toggleClass('is-active');
-    //$("#side-nav-bar")[0].style.width = "250px";
+  $("#side-nav-bar").toggleClass("hidden");
+  $("#side-nav-bar").toggleClass("side-nav-bar");
+  $("#cards").toggleClass("cards");
+  $("#filter-button").toggleClass("filter-button-active");
 }
 
 function closeNav() {
-    $("#side-nav-bar")[0].style.width = "0px";
+  $("#side-nav-bar").addClass('hidden').siblings().removeClass('side-nav-bar cards');
+  $("#filter-button").removeClass("filter-button-active");
 }
 //https://stackoverflow.com/questions/47476061/pushing-the-content-without-it-changing-width-when-opening-sidebar
+
+
+
+  $('.nav-bar-right').children().each( function( i, liElement ) {
+    var $liElement = $( liElement );
+    $liElement.click( function(e) {
+        $(this).toggleClass('nav-bar-item-selected').siblings().removeClass('nav-bar-item-selected'); 
+        e.preventDefault();
+    });
+    i++;
+  });
+
+  
+
