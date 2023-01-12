932e0633f0963b354138503b5a21d7321ff0c391
Joana <joanamartinezlopezjml@gmail.com>
2023-01-12 14:07:49 +0100

    Final commit, app completed

--- code/js/index.js
+++ code/js/index.js
@@ -1,18 +1,19 @@
 
+/* Store all the filters for the top nav bar */
 var navItemFilters = {};
 var navItemFilter;
-
+/* Store all the filters for the side nav bar */
 var sideNavItemFilters = {};
 var sideNavItemFilter;
-// quick search regex
-var qsRegex;
+/* Search bar regex to store what the user types in the input */
+var sbRegex;
 
-// init Isotope
+/* Init Isotope */
 var $cards = $('#cards-container').isotope({
   itemSelector: '.card',
   filter: function() {
     var $this = $(this);
-    var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
+    var searchResult = sbRegex ? $this.text().match( sbRegex ) : true;
     var navItemResult = navItemFilter ? $this.is( navItemFilter ) : true;
     var sideNavItemResult = sideNavItemFilter ? $(this).is( sideNavItemFilter ) : true;
     return searchResult && navItemResult && sideNavItemResult;
@@ -22,6 +23,7 @@ var $cards = $('#cards-container').isotope({
 $('.filters').on( 'click', '.nav-bar-item', function() {
   /* Top Nav Bar change element color to blue when clicked */
   $(this).addClass('nav-bar-item-selected').siblings().removeClass('nav-bar-item-selected'); 
+  /* Apply filters */
   var $navItemGroup = $(this).parents('.nav-bar-group');
   var filterGroup = $navItemGroup.attr('data-filter-group');
   navItemFilters[ filterGroup ] = $(this).attr('data-filter');
@@ -32,6 +34,7 @@ $('.filters').on( 'click', '.nav-bar-item', function() {
 $('.filters').on( 'click', '.side-nav-bar-item', function() {
   /* Side Nav Bar change element color to blue when clicked */
   $(this).addClass('side-nav-bar-item-selected').siblings().removeClass('side-nav-bar-item-selected'); 
+  /* Apply filters */
   var $sideNavItemGroup = $(this).parents('.side-nav-bar-group');
   var filterGroupSide = $sideNavItemGroup.attr('data-filter-group');
   sideNavItemFilters[ filterGroupSide ] = $(this).attr('data-filter');
@@ -39,9 +42,15 @@ $('.filters').on( 'click', '.side-nav-bar-item', function() {
   $cards.isotope();
 });
 
-// use value of search field to filter
-var $searchBar = $('.search-bar').keyup(function() {
-  qsRegex = new RegExp( $searchBar.val(), 'gi' );
+/* Use value of search bar to filter */
+var $searchBar = $('#search-bar').keyup(function() {
+  sbRegex = new RegExp( $searchBar.val(), 'gi' );
+  $cards.isotope();
+});
+
+/* Same search bar function but for the responsive vesion */
+var $mobileSearchBar = $('#search-bar-mobile').keyup(function() {
+  sbRegex = new RegExp( $mobileSearchBar.val(), 'gi' );
   $cards.isotope();
 });
 
@@ -53,21 +62,17 @@ function concatValues( obj ) {
   return value;
 }
 
-/* Open and close the side bar */
+/* Open and close the side nav bar */
 function openSideNav() {
   $("#side-nav-bar").toggleClass("hidden");
   $("#side-nav-bar").toggleClass("side-nav-bar");
-  $("#cards").toggleClass("cards");
   $("#filter-button").toggleClass("filter-button-active");
   $cards.isotope();
 }
 
 function closeSideNav() {
-  $("#side-nav-bar").addClass('hidden').siblings().removeClass('side-nav-bar cards');
+  $("#side-nav-bar").addClass('hidden').siblings().removeClass('side-nav-bar');
   $("#filter-button").removeClass("filter-button-active");
   $cards.isotope();
 }
 
-$(window).load(function() {
-  $cards.isotope();
-});
